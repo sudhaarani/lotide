fe40cd76cfbf8ad8549f06c06cf687042bf0df96
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-10-11 09:58:37 -0700

    added module.exports

--- middle.js
+++ middle.js
@@ -1,26 +1,7 @@
 /*eslint linebreak-style: ["error", "windows"]*/
-const assertArraysEqual = function (Arr1, Arr2) {
-  if (eqArrays(Arr1, Arr2)) {
-    console.log(`✅✅✅Assertion passed: [${Arr1}] === [${Arr2}]`);
-  } else {
-    console.log(`🛑🛑🛑Assertion failed: [${Arr1}] === [${Arr2}]`);
-  }
-};
-const eqArrays = function (Arr1, Arr2) {
-  //console.log(Arr1.length + "::" + Arr2.length);
-  if (Arr1.length !== Arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < Arr1.length; i++) {
-    // console.log(Arr1[i] + "::" + Arr2[i]);
-    if (Arr1[i] !== Arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
 
-//The middle function should return an array with only the middle element(s) of the provided array. This means that the length of the returned elements could vary.
+//The middle function should return an array with only the middle element(s) of the provided array. 
+//This means that the length of the returned elements could vary.
 const middle = function (Arr1) {
   let newArr = [];
   if (Arr1.length > 2 && Arr1.length % 2 === 0) { //length is even
@@ -34,10 +15,9 @@ const middle = function (Arr1) {
   }
   return newArr;
 };
-console.log(middle([1])); // no middle element so returns empty array
-console.log(middle([1, 2])); //no middle element so returns empty array
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]));
-console.log(middle([1, 2, 3, null, 5, 6, 7, 8, 9, 10]));
+// console.log(middle([1])); // no middle element so returns empty array
+// console.log(middle([1, 2])); //no middle element so returns empty array
+// console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+// console.log(middle([1, 2, 3, null, 5, 6, 7, 8, 9, 10]));
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5]);
+module.exports = middle;
