f5a02aa72c139a5164ffaad13fcb53598e4a9d8d
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-10-11 09:56:39 -0700

    added module.exports

--- head.js
+++ head.js
@@ -1,18 +1,9 @@
 /*eslint linebreak-style: ["error", "windows"]*/
-//IMPLEMENTATION OF ASSERTION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`Assertion failed: [${actual}] !== [${expected}]`);
-  }
-};
 
 //IMPLEMENTATION OF HEAD
 const head = function(arr) {
   return arr[0];
 };
 
-assertEqual(head([5]), 5);
-assertEqual(head([]), undefined);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
+
