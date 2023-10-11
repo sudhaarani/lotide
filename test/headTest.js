50505f084334333b6a7a04ded711b8afcd334ae8
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-10-11 10:08:04 -0700

    chai library function is used for testcase

---
+++ test/headTest.js
@@ -0,0 +1,20 @@
+
+const assert = require("chai").assert;
+const head = require("../head");
+
+describe("#head", function () {
+  it(" returns 1 for [1,2,3]", function () {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it(" returns '5' for ['5']", function () {
+    assert.strictEqual(head(["5"]), "5");
+  });
+  it(" returns 'Hello' for ['Hello', 'Lighthouse', 'Labs'] ", function () {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
+
+//Reference::
+// assertEqual(head([5]), 5);
+// assertEqual(head([]), undefined);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
