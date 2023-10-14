f7415445d4cceb36eba12565621acca9e4062e03
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-10-14 00:04:18 -0700

    returns count of each letter in the given string/sentence as object.

--- countLetters.js
+++ countLetters.js
@@ -1,11 +1,4 @@
 /*eslint linebreak-style: ["error", "windows"]*/
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion passed: [${actual}] === [ ${expected}]`);
-  } else {
-    console.log(`Assertion failed: [${actual}] !== [ ${expected}]`);
-  }
-};
 
 const countLetters = function (sentence) {
   let countLetter = {};
@@ -20,12 +13,6 @@ const countLetters = function (sentence) {
   }
   return countLetter;
 };
-console.log(countLetters("happy kayaking"));
-const output = countLetters("happy kayaking");
 
-assertEqual(output["p"], 2);
-assertEqual(countLetters("happy kayaking").h, 1); // can be checked in both cases
 
-assertEqual(output["k"], 2);
-
-assertEqual(output["h"], 1);
+module.exports = countLetters;
