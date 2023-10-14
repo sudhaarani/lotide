82b82273f3108d676ec3ea3e0bff47d2102a45dc
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-10-14 00:00:22 -0700

    return all the indices (zero-based positions) in the string where each character is found.

--- letterPositions.js
+++ letterPositions.js
@@ -1,26 +1,9 @@
 /*eslint linebreak-style: ["error", "windows"]*/
-//assertArraysEqual can compare complex data types like arrays.
+
 
 //We'll implement a new function letterPositions which will return all the indices (zero-based positions) 
 //in the string where each character is found.
-const assertArraysEqual = function (Arr1, Arr2) {
-  if (eqArrays(Arr1, Arr2)) {
-    console.log(`✅✅✅Assertion passed: [${Arr1}] === [${Arr2}]`);
-  } else {
-    console.log(`🛑🛑🛑Assertion failed: [${Arr1}] === [${Arr2}]`);
-  }
-};
-const eqArrays = function (Arr1, Arr2) {
-  if (Arr1.length !== Arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < Arr1.length; i++) {
-    if (Arr1[i] !== Arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+
 const letterPositions = function (sentence) {
   const results = {};
   // logic to update results here
@@ -37,9 +20,5 @@ const letterPositions = function (sentence) {
   return results;
 };
 
-console.log(letterPositions("helloworld"));
-const output = letterPositions("helloworld");
-assertArraysEqual(output["l"], [2, 3, 8]); // checking with variable which has retruned values of letterPositions
-// see these 2 difference
-assertArraysEqual(letterPositions("hello").e, [1]);// calling function
 
+module.exports = letterPositions;
